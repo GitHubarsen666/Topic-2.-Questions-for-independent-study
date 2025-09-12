b746556c03d96717dba5e6cbe906ff61e079fad8
GitHubarsen666 <150345196+GitHubarsen666@users.noreply.github.com>
2025-09-12 10:30:27 +0300

    Create 1.m

---
+++ 1.m
@@ -0,0 +1,15 @@
+% Створення часових рядів
+days 1:365;
+temperature 10+ 10*sin(2*pi*days/365) + randn(1,365); %
+сезонність + шум
+hours = 1:24;
+purchases = poissrnd(20 + 10*sin(2*pi*hours/24), 1, 24); % сезонність по годинах
+% Візуалізація
+subplot(2,1,1);
+plot(days, temperature);
+title("Температура протягом року);
+xlabel('День'); ylabel('Tемпература (°C)');
+subplot(2,1,2);
+bar(hours, purchases);
+title('Кількість покупок протягом доби);
+xlabel('Година'); ylabel('Кількість покупок");
