9217fd88ea30f9f88e6dab47ca1763c061212970
GitHubarsen666 <150345196+GitHubarsen666@users.noreply.github.com>
2025-10-06 17:40:34 +0300

    Update 2.m

--- 2.m
+++ 2.m
@@ -1,26 +1,28 @@
-% Вхідні дані: кожен рядок -спостереження, кожен стовпчик змінна
+% Вхідні дані: кожен рядок – спостереження, кожен стовпчик – змінна
 data = [20 100 30;
-25 150 20;
-30 200 10;
-35 250 5];
+        25 120 35;
+        30 150 40];
+
 % Обчислення коваріаційної матриці
 covMatrix = cov(data);
+
 % Візуалізація як теплової карти
-imagesc (covMatrix); % Створення кольорового графіка
-colorbar; % Додає шкалу кольорів
-title('Коваріаційна матриця");
-% Назва графіка
+imagesc(covMatrix);       % Створення кольорового графіка
+colorbar;                 % Додайте шкалу кольорів
+title('Коваріаційна матриця');
+
 % Підписи осей
-xticks (1:3);
+xticks(1:3);
 yticks(1:3);
-xticklabels({'Темп', 'Морозиво', 'Чай'});
-yticklabels({'Темп', 'Морозиво', 'Чай'});
+xticklabels({'Темп','Морозиво','Чай'});
+yticklabels({'Темп','Морозиво','Чай'});
+
 % Виведення чисел поверх кольорових клітинок
-for i 1:size (covMatrix, 1)
-for j = 1:size(covMatrix, 2)
-text(j, i, num2str(covMatrix(i,j), '%.2f'), ...
-'HorizontalAlignment', 'center',
-'Color', 'white',
-'FontWeight', 'bold');
-end
+for i = 1:size(covMatrix,1)
+    for j = 1:size(covMatrix,2)
+        text(j, i, num2str(covMatrix(i,j), '%.2f'), ...
+            'HorizontalAlignment', 'center', ...
+            'Color', 'white', ...
+            'FontWeight', 'bold');
+    end
 end
